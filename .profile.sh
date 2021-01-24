a35abe8b9f52f0c8c2c1c5efafbcc0a73f6c21b2
MatthewRalston <mrals89@gmail.com>
2021-01-24 14:54:28 -0500

    Ascii owl, the profile bullshit, and .signature.html for Thunderbird.

---
+++ .profile.sh
@@ -0,0 +1,86 @@
+
+
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+echo
+echo " u g dugdammuddafudda\n"
+echo
+echo "00000000000o0o00o00o00o00o0oo0oo0o0o0o00o0o0o0o0o00o\n"
+echo
+echo "nooooo no no no no no no no no no no no no no no no \n"
+echo
+echo "o o o o o o o o o o o o o o o o o o o o o o o o o o o\n"
+echo
+echo "the ghosts..."
+echo
+echo "++++++++++++++++++++++++++++++++++++++++++++++++++\n"
+echo
+echo " hoot hoot hoot \n"
+echo
+cat ~/.asciiowl.txt
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo "Moar wildlife pl0x." - sudonym
+echo
+echo
+echo
+
+echo
+
+echo
+
+echo
+
+echo
+
+echo " the watchhopper..."
+
+echo " the trash talker..."
+
+echo " the show stopper..."
+
+echo "Mr. 8Tb whats it for..."
+
+
+echo "Whatcha tellum..."
+
+echo 
+echo "the upper atmosphere"
+echo
+echo "the sublimnity"
+echo
+echo
+
+echo "What's it for?"
+
+echo "Who are we for"
+
+cat <<-EOF
+ Uh. We're bidenites of course, but more specifically I'm still a lefty, like a low down commie, no soul left inside, I hope that I get this govt grant for research purposes, and I pledge my honor, word, and mind to the service of the science at play that could touch a small area of microbial genomics. It's just a hypothesis more clearly stated this time, that's all.
+
+EOF
+echo
+
+echo "the somlemnity"
+echo
+echo "the atmospheres"
+echo
+echo "||||||||||||||||||||||||||||||||||||||||||||||||||||"
+echo
+echo "&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&"
+echo
+echo "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+echo
+echo "how do you feel?"
+echo
+echo "how much is real?"
+
+echo "-withlovefrombrokentochosen"
